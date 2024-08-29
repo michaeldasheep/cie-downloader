27acad45e796a0be0fd7e0b094034d28e7a5ce3c
Michael Zhao <michael@michaeldasheep.com>
2024-08-29 17:22:31 +1200

    added range for paperNumbers

--- version2.py
+++ version2.py
@@ -5,25 +5,49 @@ from pypdf import PdfReader
 from pypdf.errors import PdfReadError
 from os import remove
 
-def downloadLoop(siteDirectory,yearStart,yearEnd,examNumber,examSeason,season,paperNumber,variants,missingFile):
-    for year in range(yearStart,yearEnd+1):
-        print(f"Downloading {examNumber} 20{year} {season} paper {paperNumber} ms and qp")
-        paperCodes = ['ms','qp']
-        for paperCode in paperCodes:
-            for variant in range(0,variants+1):
-                if variant == 0 or variant == "0":
-                    variant = ""
-                paper = download(siteDirectory,examNumber,examSeason,year,paperCode,paperNumber,variant)
-                filePath = Path(f"./downloads/{examNumber}_{examSeason}{year}_ms_{paperNumber}{variant}.pdf")
-                filePath.write_bytes(paper.content)
-                try:
-                    PdfReader(filePath)
-                except PdfReadError:
-                    print(f"Invalid PDF file ({filePath})")
-                    missingFile.write(f"\nMISSING: {examNumber}_{examSeason}{year}_ms_{paperNumber}{variant}.pdf")
-                    remove(filePath)
-                else:
-                    pass
+def downloadLoop(siteDirectory,yearStart,yearEnd,examNumber,examSeason,season,paperNumber,paperNumberEnd,variants,missingFile):
+    if paperNumberEnd == paperNumber:
+        for year in range(yearStart,yearEnd+1):
+            print(f"Downloading {examNumber} 20{year} {season} paper {paperNumber} ms and qp")
+            paperCodes = ['ms','qp']
+            for paperCode in paperCodes:
+                for variant in range(0,variants+1):
+                    if variant == 0 or variant == "0":
+                        variant = ""
+                    paper = download(siteDirectory,examNumber,examSeason,year,paperCode,paperNumber,variant)
+                    filePath = Path(f"./downloads/{examNumber}_{examSeason}{year}_ms_{paperNumber}{variant}.pdf")
+                    filePath.write_bytes(paper.content)
+                    try:
+                        PdfReader(filePath)
+                    except PdfReadError:
+                        print(f"Invalid PDF file ({filePath})")
+                        missingFile.write(f"\nMISSING: {examNumber}_{examSeason}{year}_ms_{paperNumber}{variant}.pdf")
+                        remove(filePath)
+                    else:
+                        pass
+    elif paperNumber < paperNumberEnd:
+        for paperNumberVar in range(paperNumber,paperNumberEnd+1):
+            for year in range(yearStart,yearEnd+1):
+                print(f"Downloading {examNumber} 20{year} {season} paper {paperNumberVar} ms and qp")
+                paperCodes = ['ms','qp']
+                for paperCode in paperCodes:
+                    for variant in range(0,variants+1):
+                        if variant == 0 or variant == "0":
+                            variant = ""
+                        paper = download(siteDirectory,examNumber,examSeason,year,paperCode,paperNumberVar,variant)
+                        filePath = Path(f"./downloads/{examNumber}_{examSeason}{year}_ms_{paperNumberVar}{variant}.pdf")
+                        filePath.write_bytes(paper.content)
+                        try:
+                            PdfReader(filePath)
+                        except PdfReadError:
+                            print(f"Invalid PDF file ({filePath})")
+                            missingFile.write(f"\nMISSING: {examNumber}_{examSeason}{year}_ms_{paperNumberVar}{variant}.pdf")
+                            remove(filePath)
+                        else:
+                            pass
+    else:
+        print("ERROR")
+        exit()
 
 def main():
     configFile = open("configV2.json", "r").read()
@@ -38,14 +62,14 @@ def main():
         elif config['season'] == "m":
             examSeason = "March"
         missingFile.write(f"\n\nDOWNLOADING from 20{config['yearStart']} to 20{config['yearEnd']} {config['examNumber']} {examSeason} papers {config['paperNumber']} - Missing Files if any:")
-        downloadLoop(config['siteDirectory'],config['yearStart'],config['yearEnd'],config['examNumber'],config['season'],examSeason,config['paperNumber'],config['variants'],missingFile)
+        downloadLoop(config['siteDirectory'],config['yearStart'],config['yearEnd'],config['examNumber'],config['season'],examSeason,config['paperNumber'],config['paperNumberEnd'],config['variants'],missingFile)
         missingFile.write(f"\n\n If nothing was written above in MISSING, you are good! Redownloading may be required for other files or some papers might just not exist on the directory you are downloading from or maybe just not exist entirely.\n")
     elif (config['yearStart'] < config['yearEnd'] or config['yearStart'] == config['yearEnd']) and (config['season'] == "all"):
         seasons = {"w":"Winter","s":"Summer","m":"March"}
         for season in seasons.keys():
             examSeason = seasons[season]
             missingFile.write(f"\n\nDOWNLOADING from 20{config['yearStart']} to 20{config['yearEnd']} {config['examNumber']} {examSeason} papers {config['paperNumber']} - Missing Files if any:")
-            downloadLoop(config['siteDirectory'],config['yearStart'],config['yearEnd'],config['examNumber'],season,examSeason,config['paperNumber'],config['variants'],missingFile)
+            downloadLoop(config['siteDirectory'],config['yearStart'],config['yearEnd'],config['examNumber'],season,examSeason,config['paperNumber'],config['paperNumberEnd'],config['variants'],missingFile)
         missingFile.write(f"\n\n If nothing was written above in MISSING, you are good! Redownloading may be required for other files or some papers might just not exist on the directory you are downloading from or maybe just not exist entirely.\n")
     else:
         print("ERROR")
